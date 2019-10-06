8c66ca4ffab0d62923de3fa849c1e60d8a9bcab7
Basel Almohamad <abc@gmail.com>
2019-10-06 21:02:00 +0200

    this is second print line2ewe

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("Hallo fro Github");
+        System.out.println("Hallo fro434343 Github");
         System.out.println("Hallo fro Github");
     }
 }
