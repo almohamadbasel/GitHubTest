929dd00a1e705bc88a5a62582ca4f58c839459b1
Basel Almohamad <abc@gmail.com>
2019-10-06 21:11:21 +0200

    Hallo Hasen

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hallo fro434343wdwdswdw Github");
-        System.out.println("Hallo fro Github");
+        System.out.println("HALLO Hasan");
+
     }
 }
